7e87130050203cd1aacb68966b63b00c3f238731
Aaron Darwish <aaron7darwish@gmail.com>
2022-12-16 14:18:40 +0000

    If the guess is correct, end the loop.

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,28 @@ fn main() {
 
     println!("The secret number is: {secret_number}\nPlease input your guess.");
 
-    // Collect the answer from the user.
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-
-    // Shadow the guess variable
-    let guess: u32 = guess.trim().parse().expect("Please enter a number.");
-
-    // Inform the user about the guess value.
-    println!("You guessed: {}", guess);
-
-
-    match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small."),
-            Ordering::Greater => println!("Too big."),
-            Ordering::Equal => println!("You win."),
-        }
-
-    }
+    
+    loop {
+        // Collect the answer from the user.
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+    
+        // Shadow the guess variable
+        let guess: u32 = guess.trim().parse().expect("Please enter a number.");
+    
+        // Inform the user about the guess value.
+        println!("You guessed: {}", guess);
+    
+            match guess.cmp(&secret_number) {
+                    Ordering::Less => println!("Too small."),
+                    Ordering::Greater => println!("Too big."),
+                    Ordering::Equal => {
+                        println!("You win.");
+                        break;
+                    }
+                }
+        
+            }
+}
